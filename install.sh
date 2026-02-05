8518c6dbd1f99b113b816ab20542b9b329bdf494
Reginaldo Bertoluci Curtio <reginaldobertoluci@gmail.com>
2026-02-05 10:16:03 +0000

    add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+# Install CloudnativePG
+kubectl apply --server-side -f \
+  https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.28/releases/cnpg-1.28.0.yaml
+
+# Install Prometheus
+helm repo add prometheus-community \
+  https://prometheus-community.github.io/helm-charts
+
+helm upgrade --install \
+  -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/docs/src/samples/monitoring/kube-stack-config.yaml \
+  prometheus-community \
+  prometheus-community/kube-prometheus-stack
