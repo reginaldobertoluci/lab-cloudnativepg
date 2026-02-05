489c692be28b487b4054391ef5f55b064fe0334d
Reginaldo Bertoluci Curtio <reginaldobertoluci@gmail.com>
2026-02-05 10:40:20 +0000

    patch grafana svc to nodeport

--- patch.sh
+++ patch.sh
@@ -1,2 +1,4 @@
 kubectl patch svc prometheus-community-kube-prometheus -p '{"spec": {"type": "NodePort"}}'
 kubectl get svc prometheus-community-kube-prometheus
+kubectl patch svc prometheus-community-grafana -p '{"spec": {"type": "NodePort"}}'
+kubectl get svc prometheus-community-grafana
